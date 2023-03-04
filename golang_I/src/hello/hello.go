3746b9d287bb79c08819b12ba9bb4a205258f09c
Luiz Felipe <lffm1994@gmail.com>
2023-03-04 00:22:17 -0300

    Controle de fluxo com if

--- golang_I/src/hello/hello.go
+++ golang_I/src/hello/hello.go
@@ -16,8 +16,17 @@ func main() {
 	fmt.Println("0- Sair do Programa")
 
 	var comando int
-
-	fmt.Scanf(&comando)
+    fmt.Scan(&comando)
 	fmt.Println("O endereço da minha variavel comando é ", &comando)
 	fmt.Println("O comando escolhido foi ", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa...")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }

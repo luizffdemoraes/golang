0c8f1d8bbddbdce20c342c01a068b4793ef5f3b8
Luiz Felipe <lffm1994@gmail.com>
2023-03-04 22:41:35 -0300

    Arrays em Go

--- golang_I/src/hello/hello.go
+++ golang_I/src/hello/hello.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"net/http"
+	"os"
 )
 
 func main() {
 	exibeIntroducao()
 
-	exibeMenu()
+	for {
+		exibeMenu()
+		comando := lerComando()
 
-	comando := lerComando()
-
-	switch comando {
-	case 1:
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -48,8 +49,21 @@ func lerComando() int {
 }
 
 func iniciarMonitoramento() {
-    fmt.Println("Monitorando...")
-    site := "https://www.alura.com.br"
-    resp, _ := http.Get(site)
-    fmt.Println(resp)
+	fmt.Println("Monitorando...")
+
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com/"
+	sites[1] = "https://www.alura.com.br"
+	sites[2] = "https://www.caelum.com.br"
+
+	fmt.Println(sites)
+
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }

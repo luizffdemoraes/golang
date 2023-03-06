9e90a8fae2d048ad485cbca6d9d0d467e8d3ff03
Luiz Felipe <lffm1994@gmail.com>
2023-03-06 02:05:09 -0300

    Formatando tempo em Go

--- golang_I/src/hello/hello.go
+++ golang_I/src/hello/hello.go
@@ -146,7 +146,9 @@ func registraLog(site string, status bool) {
 		fmt.Println("Ocorreu um erro:", err)
 	}
 
-	arquivo.WriteString(site + " - online: " + strconv.FormatBool(status) + "\n")
+	// https://go.dev/src/time/format.go
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site +
+		" - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
